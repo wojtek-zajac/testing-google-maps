6497b277c3724ba96bee1bdd59ea4cf0d793bf42
Wojciech Zając <wojciechzajaaac@gmail.com>
2018-08-13 20:59:48 +0200

    Style map and markers

--- app.js
+++ app.js
@@ -3,10 +3,79 @@ let map;
 let markers = [];
 
 function initMap() {
+    var styles = [
+        {
+          featureType: 'water',
+          stylers: [
+            { color: '#19a0d8' }
+          ]
+        },{
+          featureType: 'administrative',
+          elementType: 'labels.text.stroke',
+          stylers: [
+            { color: '#ffffff' },
+            { weight: 6 }
+          ]
+        },{
+          featureType: 'administrative',
+          elementType: 'labels.text.fill',
+          stylers: [
+            { color: '#e85113' }
+          ]
+        },{
+          featureType: 'road.highway',
+          elementType: 'geometry.stroke',
+          stylers: [
+            { color: '#efe9e4' },
+            { lightness: -40 }
+          ]
+        },{
+          featureType: 'transit.station',
+          stylers: [
+            { weight: 9 },
+            { hue: '#e85113' }
+          ]
+        },{
+          featureType: 'road.highway',
+          elementType: 'labels.icon',
+          stylers: [
+            { visibility: 'off' }
+          ]
+        },{
+          featureType: 'water',
+          elementType: 'labels.text.stroke',
+          stylers: [
+            { lightness: 100 }
+          ]
+        },{
+          featureType: 'water',
+          elementType: 'labels.text.fill',
+          stylers: [
+            { lightness: -100 }
+          ]
+        },{
+          featureType: 'poi',
+          elementType: 'geometry',
+          stylers: [
+            { visibility: 'on' },
+            { color: '#f0e4d3' }
+          ]
+        },{
+          featureType: 'road.highway',
+          elementType: 'geometry.fill',
+          stylers: [
+            { color: '#efe9e4' },
+            { lightness: -25 }
+          ]
+        }
+    ];
+
     // Constructor creates a new map - only center and zoom are required.
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 40.7413549, lng: -73.9980244},
-        zoom: 13
+        zoom: 13,
+        styles: styles,
+        mapTypeControl: true
     });
     // These are the real estate listings that will be shown to the user.
     // Normally we'd have these in a database instead.
@@ -18,20 +87,29 @@ function initMap() {
         {title: 'TriBeCa Artsy Bachelor Pad', location: {lat: 40.7195264, lng: -74.0089934}},
         {title: 'Chinatown Homey Space', location: {lat: 40.7180628, lng: -73.9961237}}
     ];
+
     let largeInfowindow = new google.maps.InfoWindow();
-    let bounds = new google.maps.LatLngBounds();
+
+    // Style the markers a bit. This will be our listing marker icon.
+    let defaultIcon = makeMarkerIcon('0091ff');
+
+    // Create a "highlighted location" marker color for when the user
+    // mouses over the marker.
+    let highlightedIcon = makeMarkerIcon('ffff24');
+
+    // let largeInfowindow = new google.maps.InfoWindow();
+    // let bounds = new google.maps.LatLngBounds();
     // The following group uses the location array to create an array of markers on initialize.
     for (let i = 0; i < locations.length; i++) {
         // Get the position from the location array.
         let position = locations[i].location;
-        let lat = position.lat;
-        // let lng
         let title = locations[i].title;
         // Create a marker per location, and put into markers array.
         let marker = new google.maps.Marker({
             position: position,
             title: title,
             animation: google.maps.Animation.DROP,
+            icon: defaultIcon,
             id: i
         });
         // Push the marker to our array of markers.
@@ -39,6 +117,14 @@ function initMap() {
         // Create an onclick event to open an infowindow at each marker.
         marker.addListener('click', function() {
         populateInfoWindow(this, largeInfowindow);
+        });
+        // Two event listeners - one for mouseover, one for mouseout,
+        // to change the colors back and forth.
+        marker.addListener('mouseover', function() {
+            this.setIcon(highlightedIcon);
+        });
+        marker.addListener('mouseout', function() {
+            this.setIcon(defaultIcon);
         });
     }
 
@@ -78,4 +164,18 @@ function hideListings() {
     for (let i = 0; i < markers.length; i++) {
         markers[i].setMap(null);
     }
+}
+
+// This function takes in a COLOR, and then creates a new marker
+// icon of that color. The icon will be 21 px wide by 34 high, have an origin
+// of 0, 0 and be anchored at 10, 34.
+function makeMarkerIcon(markerColor) {
+    let markerImage = new google.maps.MarkerImage(
+        'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
+        '|40|_|%E2%80%A2',
+        new google.maps.Size(21, 34),
+        new google.maps.Point(0, 0),
+        new google.maps.Point(10, 34),
+        new google.maps.Size(21,34));
+    return markerImage;
 }
