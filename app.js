6dbea84250a2b6e441734f5789a409e3f72069cd
Wojciech Zając <wojciechzajaaac@gmail.com>
2018-08-13 19:23:21 +0200

    Show multiple markers and infowindows that fit screen

--- app.js
+++ app.js
@@ -1,27 +1,62 @@
 let map;
+// Create a new blank array for all the listing markers.
+let markers = [];
 
 function initMap() {
-    // Contructor creates a new map - only center and zoom are required.
-    // First specify where on the HTML to load the map (first parameter):
+    // Constructor creates a new map - only center and zoom are required.
     map = new google.maps.Map(document.getElementById('map'), {
-    // Second parameter - what part of the world to show:
-        center: {lat: 40.7413549, lng: -73.9980244}, // New York
-        // center: {lat: 50.083086, lng: 20.015407}, // Nowa Huta District, Poland
-        // The highr the number, the more details. You can go up to 22.
+        center: {lat: 40.7413549, lng: -73.9980244},
         zoom: 13
     });
-    let tribeca = {lat: 40.719526, lng: -74.0089934};
-    let marker = new google.maps.Marker({
-        position: tribeca,
-        map: map,
-        title: 'My first Marker!',
-        draggable: true,
-        // animation: google.maps.Animation.BOUNCE
-    });
-    let infowindow = new google.maps.InfoWindow({
-        content: 'Do you ever feel like an InfoWindow, floating through the wind, ready to start again?'
-    });
-    marker.addListener('click', function() {
+    // These are the real estate listings that will be shown to the user.
+    // Normally we'd have these in a database instead.
+    let locations = [
+        {title: 'Park Ave Penthouse', location: {lat: 40.7713024, lng: -73.9632393}},
+        {title: 'Chelsea Loft', location: {lat: 40.7444883, lng: -73.9949465}},
+        {title: 'Union Square Open Floor Plan', location: {lat: 40.7347062, lng: -73.9895759}},
+        {title: 'East Village Hip Studio', location: {lat: 40.7281777, lng: -73.984377}},
+        {title: 'TriBeCa Artsy Bachelor Pad', location: {lat: 40.7195264, lng: -74.0089934}},
+        {title: 'Chinatown Homey Space', location: {lat: 40.7180628, lng: -73.9961237}}
+    ];
+    let largeInfowindow = new google.maps.InfoWindow();
+    let bounds = new google.maps.LatLngBounds();
+    // The following group uses the location array to create an array of markers on initialize.
+    for (let i = 0; i < locations.length; i++) {
+        // Get the position from the location array.
+        let position = locations[i].location;
+        let title = locations[i].title;
+        // Create a marker per location, and put into markers array.
+        let marker = new google.maps.Marker({
+            map: map,
+            position: position,
+            title: title,
+            animation: google.maps.Animation.DROP,
+            id: i
+        });
+        // Push the marker to our array of markers.
+        markers.push(marker);
+        // Create an onclick event to open an infowindow at each marker.
+        marker.addListener('click', function() {
+        populateInfoWindow(this, largeInfowindow);
+        });
+        bounds.extend(markers[i].position);
+    }
+    // Extend the boundaries of the map for each marker
+    map.fitBounds(bounds);
+}
+
+// This function populates the infowindow when the marker is clicked. We'll only allow
+// one infowindow which will open at the marker that is clicked, and populate based
+// on that markers position.
+function populateInfoWindow(marker, infowindow) {
+    // Check to make sure the infowindow is not already opened on this marker.
+    if (infowindow.marker != marker) {
+        infowindow.marker = marker;
+        infowindow.setContent('<div>' + marker.title + '</div>');
         infowindow.open(map, marker);
-    });
+        // Make sure the marker property is cleared if the infowindow is closed.
+        infowindow.addListener('closeclick',function(){
+        infowindow.setMarker = null;
+        });
+    }
 }
